574a61967c59c70e21f9f492659b95d58becb812
云影 <zhangcaiqian@outlook.com>
2020-06-08 09:00:12 +0800

    feat: selection sort
    算法描述：从未排序的序列中，先找到最大值或者最小值，找到之后和参考值换位，如果找到最小值，则将其移到数组前面，如果找的是最大值，将其移到数组最后。对剩余的元素重复此过程。

--- src/index.js
+++ src/index.js
@@ -1,16 +1,35 @@
 function bubbleSort(arr) {
   var len = arr.length;
-  for(var i = 0; i < len - 1; i++) {
-      for(var j = 0; j < len - 1 - i; j++) {
-          if(arr[j] > arr[j+1]) {        // 相邻元素两两对比
-              var temp = arr[j+1];        // 元素交换
-              arr[j+1] = arr[j];
-              arr[j] = temp;
-          }
-          console.log(j);
+  for (var i = 0; i < len - 1; i++) {
+    for (var j = 0; j < len - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        // 相邻元素两两对比
+        var temp = arr[j + 1]; // 元素交换
+        arr[j + 1] = arr[j];
+        arr[j] = temp;
       }
+    }
   }
   return arr;
 }
 
-console.log(bubbleSort([]));
+console.log(bubbleSort([1, 2, 355, 3, 7, 4]));
+
+function selectionSort(arr) {
+  const len = arr.length;
+  let minIndex, tempItem;
+  for (let i = 0; i < len; i++) {
+    minIndex = i;
+    for (let j = i + 1; j < len; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+    tempItem = arr[i];
+    arr[i] = arr[minIndex];
+    arr[minIndex] = tempItem;
+  }
+  return arr;
+}
+
+console.log(selectionSort([1, 2, 355, 3, 7, 4]));
