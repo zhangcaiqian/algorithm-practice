b6685015e9fd01a98e0dcfde011846ee03ca635b
zhouqingyi01 <qingyi.zhou@dragonplus.com>
2021-07-21 23:24:11 +0800

    AST 简单实现

--- ast.js
+++ ast.js
@@ -1,51 +1,4 @@
-### 定义
-抽象语法树AST（Abstract Syntax Tree，AST）是语法树的抽象表示，它以树的形式表现语法结构，树上的每个节点都代表一个语法结构。
-
-AST 测试平台：[测试](https://astexplorer.net/)
-
-### acorn
-JS用acorn解析抽象语法树， ESLint、Babel、Vue.js都依赖acorn。babel 也依赖acorn，babel compile的流程如下：
-
-* 解析: 将代码(其实就是字符串)转换成 AST( 抽象语法树)
-* 转换: 访问 AST 的节点进行变换操作生成新的 AST
-* 生成: 以新的 AST 为基础生成代
-解析流程如下图：
-![测试](https://github.com/zhangcaiqian/algorithm-practice/blob/master/Assets/acorn.png)
-
-
-acorn 将词法分析和语法分析交替进行，只需要扫描一遍代码即可得到最终 AST 结果。
-
-------------------------------------
-### 实现一个简单 acorn parser
-关键步骤：
-1. 词法分析，将代码分割成token流，即语法数组
-2. 语法分析，将语法数组变成树状结构
-
-1. 词法分析哪些需要处理
-    1. 科学计数法和数组
-    2. ()
-    3. 标识符、变量、关键字、运算符
-    4. 注释符，中括号
-
-
-词法分析代码：
-例：
-```
-const add = (a, b) => a + b
-```
-
-期望的结果
-```
-[
-  { type: "identifier", value: "const" },
-  { type: "whitespace", value: " " },
-  ...
-]
-```
-词法分析器实现代码：
-```
 // 词法分析器，接收字符返回token数组
-export // 词法分析器，接收字符返回token数组
 const tokenizer = (code) => {
     // 储存 token 数组
     const tokens = [];
@@ -152,4 +105,3 @@ const tokenizer = (code) => {
 }
 let code = "const add = (a, b) => a + b";
 console.log(tokenizer(code));
-```
